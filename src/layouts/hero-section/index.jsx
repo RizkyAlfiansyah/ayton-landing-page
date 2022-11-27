30fc45604d5d08566bb2ee35e2fad546cdd9f998
Rizky Alfiansyah <rizkkyaf@gmail.com>
2022-11-27 23:44:30 +0800

    update again

--- src/layouts/hero-section/index.jsx
+++ src/layouts/hero-section/index.jsx
@@ -13,11 +13,13 @@ const Hero = ({ onClick }) => {
                 <br />
                 building your business
             </p>
-            <p className="md:hidden w-80 sm:w-auto xl:text-56 xl:leading-78 tracking-wider md:text-40 md:leading-48 text-32 leading-40 text-center md:font-medium font-caslon font-normal lg:p-0 text-white 2xl:mt-12 mt-28">
-                We focus on <br /> fundraising so
-                <br className='lg:hidden' />
-                {" "}you can focus <br /> on building <br /> your business
-            </p>
+            <div className="md:hidden sm:w-auto xl:text-56 xl:leading-78 tracking-wider md:text-40 md:leading-48 text-32 leading-40 text-center md:font-medium font-caslon font-normal lg:p-0 text-white 2xl:mt-12 mt-24 bg-black bg-opacity-40 rounded-lg p-4">
+                <p>
+                    We focus on <br /> fundraising so
+                    <br className='lg:hidden' />
+                    {" "}you can focus <br /> on building <br /> your business
+                </p>
+            </div>
         </>
     )
 }
